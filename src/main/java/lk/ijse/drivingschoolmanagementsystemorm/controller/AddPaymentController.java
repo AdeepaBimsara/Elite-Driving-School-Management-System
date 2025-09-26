22ba95bef2192a479d08676c131299f33a336de0
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2025-09-26 09:23:18 +0530

    fixed ui

--- src/main/java/lk/ijse/drivingschoolmanagementsystemorm/controller/AddPaymentController.java
+++ src/main/java/lk/ijse/drivingschoolmanagementsystemorm/controller/AddPaymentController.java
@@ -3,22 +3,25 @@ package lk.ijse.drivingschoolmanagementsystemorm.controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.drivingschoolmanagementsystemorm.util.NavigationUtil;
 
 public class AddPaymentController {
     public AnchorPane ancAddPayment;
     public Button btnBack;
     public Label lblPaymentID;
-    public ComboBox cmbStudent;
-    public ListView courseListView;
-    public TextField txtAmount;
+    public ComboBox<String> cmbStudent;
+    public ListView<String> courseListView;
+    public Label lblAmount;
+    public TextField txtAmountPaid;
     public DatePicker dpDate;
-    public ComboBox cmbStatus;
+    public ComboBox<String> cmbStatus;
     public Button btnSave;
     public Button btnDelete;
     public Button btnUpdate;
     public Button btnCancel;
 
     public void btnBack(ActionEvent actionEvent) {
+        NavigationUtil.navigateTo(ancAddPayment, "/view/payment/PaymentPage.fxml");
     }
 
     public void btnSave(ActionEvent actionEvent) {
